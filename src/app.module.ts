dbe715acbe3ec4c2cf4acc317d8c107308392621
Tom Jannes <tom.jannes@gmail.com>
2018-03-10 16:22:50 +0100

    Add sku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
+import { SkuModule } from './sku/sku.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://developerx:test@127.0.0.1:27017/NGInvoicing'),
-    CustomerModule
+    CustomerModule,
+    SkuModule
   ],
   controllers: [],
   components: [],
