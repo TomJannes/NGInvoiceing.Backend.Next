c852166eea43de12e1836ea8e2f94f3e3b3a7ea4
Tom Jannes <tom.jannes@gmail.com>
2018-03-26 20:23:48 +0200

    Major refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApplicationModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as path from 'path';
-import * as mm from 'mongodb-migrations';
+import { Migrator } from './migrations/migrator';
 
 async function bootstrap() {
-	var migrator = new mm.Migrator({
-		'url': 'mongodb://127.0.0.1:27017/NGInvoicing'
-	});
-	var migrationPath = path.join(__dirname, 'migrations');
-    migrator.runFromDir(migrationPath, function(err, result){
-        console.warn(err)
-    });
+	var migrator = new Migrator();
+	migrator.migrate();
 
 	const app = await NestFactory.create(ApplicationModule);
 
